a9f6c1ecb9df2e79ef1ff4c20efdc184cae8c8a3
songmin <tft0720@gmail.com>
2022-01-11 22:03:07 +0900

    selector 출력 변형 렌더

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,4 +1,4 @@
-import { atom } from 'recoil';
+import { atom, selector } from 'recoil';
 
 export interface IToDo {
   text: string;
@@ -9,4 +9,27 @@ export interface IToDo {
 export const toDoState = atom<IToDo[]>({
   key: 'toDo',
   default: [],
+});
+
+// => atom 이 변화면 selector 변함
+
+// export const toDoSelector = selector({
+//   key: 'toDoSelecter',
+//   get: ({ get }) => {
+//     const toDos = get(toDoState);
+//     return toDos.length;
+//   },
+// });
+
+// ⭐️ state 자체를 바꾸는게 아니라, output을 바꾸고 있다는 점 인지
+export const toDoSelector = selector({
+  key: 'toDoSelecter',
+  get: ({ get }) => {
+    const toDos = get(toDoState);
+    return [
+      toDos.filter((toDo) => toDo.category === 'TO_DO'), //
+      toDos.filter((toDo) => toDo.category === 'DOING'),
+      toDos.filter((toDo) => toDo.category === 'DONE'),
+    ];
+  },
 });
--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,20 +1,36 @@
 import { useRecoilValue } from 'recoil';
-import { toDoState } from '../atoms';
+import styled from 'styled-components';
+import { toDoSelector, toDoState } from '../atoms';
 import CreateToDo from './CreateToDo';
 import ToDo from './ToDo';
 
 function ToDoList() {
   // const value = useRecoilValue(toDoState);
   // const modFn = useSetRecoilState(toDoState);
-  const toDos = useRecoilValue(toDoState);
-  console.log(toDos);
+  const [toDos, doing, done] = useRecoilValue(toDoSelector);
+
   return (
     <div style={{ padding: '50px' }}>
       <h1>To Dos</h1>
       <hr />
       <CreateToDo />
+      <Title>💡To Do</Title>
       <ul>
         {toDos.map((toDo) => (
+          <ToDo key={toDo.id} {...toDo} />
+        ))}
+      </ul>
+      <hr />
+      <Title>Doing</Title>
+      <ul>
+        {doing.map((toDo) => (
+          <ToDo key={toDo.id} {...toDo} />
+        ))}
+      </ul>
+      <hr />
+      <Title>Done</Title>
+      <ul>
+        {done.map((toDo) => (
           <ToDo key={toDo.id} {...toDo} />
         ))}
       </ul>
@@ -23,3 +39,7 @@ function ToDoList() {
 }
 
 export default ToDoList;
+
+const Title = styled.h2`
+  margin-top: 10px;
+`;
